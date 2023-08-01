80197fa92e13d97e37d90ae89a82674b6513ed92
b-abhiraj <94778209+b-abhiraj@users.noreply.github.com>
2023-08-02 01:04:55 +0530

    Update HomePage.js

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -69,10 +69,10 @@ const Homepage = () => {
                                             <div className='flex mr-5'>
                                                 <p>{p.body}</p>
                                                 <div className='right-0 ml-5 group absolute'><MoreVertical />
-                                                    <div className='hidden absolute group-hover:flex bg-white flex-col gap-5 rounded-md shadow-md p-3'>
+                                                    <div className='hidden absolute group-hover:flex bg-white dark:text-white flex-col gap-5 rounded-md shadow-md p-3'>
                                                         <ul>
                                                             <li>
-                                                                <button className='hover:bg-blue-800 hover:text-white p-2 rounded-md' onClick={showModal}>  Update
+                                                                <button className='hover:bg-blue-800 hover:text-white  p-2 rounded-md' onClick={showModal}>  Update
                                                                 </button>
                                                                 <Modal title="Update a Note" open={isModalOpen} onOk={() => {
                                                                     try {
